6189aa03822763fb7c6e0667945e00740aa349c5
slmanasad <slman.me@gmail.com>
2019-02-16 12:50:41 +0500

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const config = require('config');
+const helmet = require('helmet');
+const morgan = require('morgan');
 const Joi = require('joi');
 const express = require('express');
 const app = express();
 const logging = require("./logger");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));  //parses incoming request with url ecoded payload(your data is                                                        send in url encoded form like key=value&key=value... Data is send                                                    in url as key value pairs)
+app.use(helmet());
+app.use(morgan('tiny'));
+
+
+// Configuration
+console.log("Application name " + config.get('name'));
+console.log("Mail Server " + config.get('mail.host'));
+console.log("Mail Password " + config.get('mail.password'));
 
 
 // custom middleware
